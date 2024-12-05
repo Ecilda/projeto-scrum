c2cfc77edbc014bf3c95260c6822ce37cdc2e9a9
malugarcia <barcelosecilda@gmail.com>
2024-12-05 11:26:46 -0300

    primeira versão

---
+++ src/main/java/com/fidelles/entidade/Aluno.java
@@ -0,0 +1,37 @@
+package com.fidelles.entidade;
+
+public class Aluno {
+    private int matricula;
+    private String nome;
+    private String cpf;
+    private String email;
+    private String telefone;
+
+
+    public int getMatricula() {
+        return matricula;
+    }
+    public void setMatricula(int matricula) {
+        this.matricula = matricula
+    }
+    public String getNome(){
+        return nome;
+    }
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+    public String getcpf(){
+        return cpf;
+    }
+    public void setCpf(String cpf){
+        this.cpf = cpf;
+    }
+    public String getEmail(){
+        return cpf;
+    }
+    public void setEmail(String cpf){
+        this.email = email;
+    }
+    public String telefone
+    }
+}
